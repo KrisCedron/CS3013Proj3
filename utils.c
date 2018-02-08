48e45874e9f89b811ed01840dcd8db9804048ca0
ccedron <ccedron@CC-Linux>
2018-02-07 22:09:16 -0500

    Added Utils unit with a func to get a rand num follow stand dist

---
+++ utils.c
@@ -0,0 +1,12 @@
+/*
+ * utils.c
+ *
+ *  Created on: Feb 7, 2018
+ *      Author: ccedron
+ */
+#include "utils.h"
+#include <math.h>
+
+int getNormDistNum(double mean, double stdDev){
+	return sqrt(-2 * log(a)) * cos(2 *M_PI*rand());
+}
